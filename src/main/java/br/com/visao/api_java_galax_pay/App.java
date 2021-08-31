8aea93791052ea9b6f0124ccb634065c818176cf
Wilton Oliveira <79815643+wiltonOA@users.noreply.github.com>
2021-08-30 23:06:38 -0300

    Atualizado a classe App

--- src/main/java/br/com/visao/api_java_galax_pay/App.java
+++ src/main/java/br/com/visao/api_java_galax_pay/App.java
@@ -1,15 +1,13 @@
-package br.com.visao.api_java_galax_pay;
-import br.com.visao.api_java_galax_pay.controller.CobrancaController;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package br.com.visao.API_GALAXPAY;
 
 /**
  *
- * @author WILTON
+ * @author WILTON OLIVEIRA
  */
 public class App {
 
@@ -17,15 +15,15 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	
+        
         String token = new Autentica().geraToken();
-        System.err.println("TOKEN: "+token);
+        System.out.println("TOKEN: " + token);
 
-       String cobranca = new CobrancaController().geraCobranca();   
-       System.err.println("COBRANCA:"+cobranca);
-       
-       String cobrancaGerada = new EnviaCobrancaAPI().EnviaCobranca(token, cobranca);
-       System.err.println("RETORNO: "+cobrancaGerada);
+        String cobranca = new CobrancaController().insereDadosCobranca();
+        System.out.println("COBRANCA:" + cobranca);
+
+        String cobrancaGerada = new EnviaCobrancaAPI().EnviaCobranca(token, cobranca);
+        System.out.println("RETORNO: " + cobrancaGerada);
     }
 
 }
